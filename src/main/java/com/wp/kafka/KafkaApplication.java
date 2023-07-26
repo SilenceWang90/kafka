2795badd0ad1b2eba8a7ff97e02c2cd1a0234f1d
Wangpeng@90 <wangpeng0717@sina.cn>
2023-07-26 13:37:11 +0800

    Kafka

--- src/main/java/com/wp/kafka/KafkaApplication.java
+++ src/main/java/com/wp/kafka/KafkaApplication.java
@@ -1,9 +1,17 @@
 package com.wp.kafka;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
 /**
  * @Description
  * @Author admin
  * @Date 2023/7/22 9:35
  */
+@SpringBootApplication
 public class KafkaApplication {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = SpringApplication.run(KafkaApplication.class, args);
+    }
 }
