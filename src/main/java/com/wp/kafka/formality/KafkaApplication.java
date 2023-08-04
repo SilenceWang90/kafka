c88f536881be6acc00afff25cff424ce62a53131
Wangpeng@90 <wangpeng0717@sina.cn>
2023-08-04 11:49:25 +0800

    Kafka

---
+++ src/main/java/com/wp/kafka/formality/KafkaApplication.java
@@ -0,0 +1,16 @@
+package com.wp.kafka.formality;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description
+ * @Author admin
+ * @Date 2023/8/4 11:23
+ */
+@SpringBootApplication
+public class KafkaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaApplication.class, args);
+    }
+}
